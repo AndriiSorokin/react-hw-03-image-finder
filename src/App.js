4ea2964653a7048cce629925c606b3f02e17b301
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-02-28 18:53:11 +0200

    make search bar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Searchbar from './components/Searchbar/Searchbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Searchbar />
+      </div>
+    );
+  }
+}
+
+export default App;
